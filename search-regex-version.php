447022c2576fb6c184d1f6c4e9e1fe111f12106d
John Godley <johng75@gmail.com>
2020-05-11 08:09:32 +0100

    Bundle 2.0.1

--- search-regex-version.php
+++ search-regex-version.php
@@ -1,5 +1,5 @@
 <?php
 
 define( 'SEARCHREGEX_VERSION', '2.0.1' );
-define( 'SEARCHREGEX_BUILD', '8699e97dd46ac79ecf80c6254c1a2bb7' );
+define( 'SEARCHREGEX_BUILD', '988f594fdf195fa7746f1dc82eb2f821' );
 define( 'SEARCHREGEX_MIN_WP', '4.6' );
