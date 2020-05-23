d0b420cfbde36381423e8f148e8a8778077d30a1
John Godley <johng75@gmail.com>
2020-05-23 19:06:14 +0100

    Bundle 2.1 beta 3

--- search-regex-version.php
+++ search-regex-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'SEARCHREGEX_VERSION', '2.1-beta-2' );
-define( 'SEARCHREGEX_BUILD', '0f41c98d2aeefdf18d6df57bcf677824' );
+define( 'SEARCHREGEX_VERSION', '2.1-beta-3' );
+define( 'SEARCHREGEX_BUILD', 'ee41b9ebc47086b797918c02607943da' );
 define( 'SEARCHREGEX_MIN_WP', '4.6' );
