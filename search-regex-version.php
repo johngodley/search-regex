d8fec4d121686d4315c11a32c58984196c656797
John Godley <johng75@gmail.com>
2020-07-18 11:00:53 +0100

    Update packages and rebundle 2.2

--- search-regex-version.php
+++ search-regex-version.php
@@ -1,5 +1,5 @@
 <?php
 
 define( 'SEARCHREGEX_VERSION', '2.2' );
-define( 'SEARCHREGEX_BUILD', '6f40b6ca6ef554f6b6c89b33571e8e7e' );
+define( 'SEARCHREGEX_BUILD', '23c3ed58e4ec31ca08c98a86cde8ad96' );
 define( 'SEARCHREGEX_MIN_WP', '4.6' );
