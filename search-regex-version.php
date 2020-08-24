27d94201070c0d301ba01635ba7ad2f54a2320b9
John Godley <johng75@gmail.com>
2020-08-24 10:25:52 +0100

    Bump to 2.3 beta 1

--- search-regex-version.php
+++ search-regex-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'SEARCHREGEX_VERSION', '2.2.1' );
+define( 'SEARCHREGEX_VERSION', '2.3-beta-1' );
 define( 'SEARCHREGEX_BUILD', '01db47af2e29c6f6dba79015747287a1' );
 define( 'SEARCHREGEX_MIN_WP', '4.6' );
