8bfdb1d1cf1f7397b62543b1c950322c6f3ace44
John <john@johngodley.com>
2026-01-03 14:56:35 +0000

    Version bump for 3.3 (#207)

--- search-regex.php
+++ search-regex.php
@@ -15,8 +15,8 @@ For full license details see license.txt
 
 define( 'SEARCHREGEX_FILE', __FILE__ );
 
-// This file must support PHP < 7.2 so as not to crash
-if ( version_compare( phpversion(), '7.2' ) < 0 ) {
+// This file must support PHP < 7.4 so as not to crash
+if ( version_compare( phpversion(), '7.4' ) < 0 ) {
 	// @phpstan-ignore-next-line
 	add_filter( 'plugin_action_links_' . basename( dirname( SEARCHREGEX_FILE ) ) . '/' . basename( SEARCHREGEX_FILE ), 'searchregex_deprecated_php', 10, 4 );
 
@@ -28,7 +28,7 @@ if ( version_compare( phpversion(), '7.2' ) < 0 ) {
 	 */
 	function searchregex_deprecated_php( $links ) {
 		/* translators: 1: server PHP version. 2: required PHP version. */
-		array_unshift( $links, '<a href="https://searchregex.com/support/problems/php-version/" style="color: red; text-decoration: underline">' . sprintf( __( 'Disabled! Detected PHP %1$s, need PHP %2$s+', 'search-regex' ), phpversion(), '5.6' ) . '</a>' );
+		array_unshift( $links, '<a href="https://searchregex.com/support/problems/php-version/" style="color: red; text-decoration: underline">' . sprintf( __( 'Disabled! Detected PHP %1$s, need PHP %2$s+', 'search-regex' ), phpversion(), '7.4' ) . '</a>' );
 		return $links;
 	}
 
