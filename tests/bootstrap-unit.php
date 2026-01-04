14d5238dc1e9590ff70462e7081ba85ef6792b43
John Godley <john@johngodley.com>
2026-01-04 15:26:15 +0000

    Fix jest errors

--- tests/bootstrap-unit.php
+++ tests/bootstrap-unit.php
@@ -5,4 +5,7 @@ require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 define( 'PLUGIN_PATH', dirname( __DIR__ ) );
 define( 'SEARCHREGEX_TESTS', true );
 
-require PLUGIN_PATH . '/tests/unit-test.php';
+// Only load Brain\Monkey TestCase if Brain\Monkey is available
+if ( class_exists( 'Brain\Monkey' ) ) {
+	require PLUGIN_PATH . '/tests/unit-test.php';
+}
