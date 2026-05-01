9038cdf95d764da94fe61342300f8b379ab039d8
John Godley <john@johngodley.com>
2026-05-01 16:50:00 +0100

    Copy joins in Query::add_select_only
    When merging select-only queries, the source query's joins were dropped, so any join the source relied on (e.g. term description) was missing from the resulting SQL.

--- includes/sql/class-query.php
+++ includes/sql/class-query.php
@@ -175,6 +175,7 @@ class Query {
 	 */
 	public function add_select_only( Query $query ) {
 		$this->select = array_merge( $this->select, $query->select );
+		$this->joins = array_merge( $this->joins, $query->joins );
 	}
 
 	/**
